9b949cd9ca575aac457c6d1a032fd597196ce1ac
Matheus Reis <matheusnunes3@gmail.com>
2025-07-20 09:08:07 +0000

    ajuste codigo

--- DesafioSupertrunfo.c
+++ DesafioSupertrunfo.c
@@ -14,59 +14,59 @@ int populacao2, pontos2;
 float area2, pib2;
 
    
-   //Entrada de Dados - Carta 1
-   printf("Canoas\n");
+//Entrada de Dados - Carta 1
+printf("Canoas\n");
 
-   printf("Digite a Populacao: ");
-   scanf ("%d",&populacao1);
+printf("Digite a Populacao: ");
+scanf ("%d",&populacao1);
 
-   printf("Digite Area: ");
-   scanf ("%f",&area1);
+printf("Digite Area: ");
+scanf ("%f",&area1);
 
-   printf("Digite o Pib: ");
-   scanf ("%f",&pib1);
+printf("Digite o Pib: ");
+scanf ("%f",&pib1);
 
-   printf("Digite o numero de Pontos Turisticos: ");
-   scanf ("%d",&pontos1);
+printf("Digite o numero de Pontos Turisticos: ");
+scanf ("%d",&pontos1);
 
-   printf("\n");
+printf("\n");
 
-   //Entrada de Dados - Carta 2
-   printf("Alvorada\n");
+//Entrada de Dados - Carta 2
+printf("Alvorada\n");
 
-   printf("Digite a Populacao: ");
-   scanf ("%d",&populacao2);
+printf("Digite a Populacao: ");
+scanf ("%d",&populacao2);
 
-   printf("Digite Area: ");
-   scanf ("%f",&area2);
+printf("Digite Area: ");
+scanf ("%f",&area2);
 
-   printf("Digite o Pib: ");
-   scanf ("%f",&pib2);
+printf("Digite o Pib: ");
+scanf ("%f",&pib2);
 
-   printf("Digite o numero de Pontos Turisticos: ");
-   scanf ("%d",&pontos2);
+printf("Digite o numero de Pontos Turisticos: ");
+scanf ("%d",&pontos2);
 
-   printf("\n");
+printf("\n");
 
-   // Exibição - Carta 1
-       printf("=== Canoas ===\n");
-           printf("Populacao: %d habitantes\n", populacao1);
-               printf("Area: %.2f km\n", area1);
-                   printf("PIB: %.2f bilhoes de reais\n", pib1);
-                       printf("Numero de Pontos Turisticos: %d\n", pontos1);
+// Exibição - Carta 1
+    printf("=== Canoas ===\n");
+    printf("Populacao: %d habitantes\n", populacao1);
+    printf("Area: %.2f km\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontos1);
 
-                       printf("\n");
-                           
-                           // Exibição - Carta 2
-                               printf("=== Alvorada ===\n");
-                                   printf("Populacao: %d habitantes\n", populacao2);
-                                       printf("Area: %.2f km\n", area2);
-                                           printf("PIB: %.2f bilhoes de reais\n", pib2);
-                                               printf("Numero de Pontos Turisticos: %d\n", pontos2);
+printf("\n");
+    
+// Exibição - Carta 2
+    printf("=== Alvorada ===\n");
+    printf("Populacao: %d habitantes\n", populacao2);
+    printf("Area: %.2f km\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontos2);
 
-                                               printf("\n");
+printf("\n");
 
-                                               return 0;
+return 0;
 
-                                               }
-                                                
+}
+ 
